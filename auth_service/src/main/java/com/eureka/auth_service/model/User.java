ef034297b980991cc7186894363829e7aba7b247
panchaliSam <panchalisam20030117@gmail.com>
2025-08-06 17:20:45 +0530

    Implemet user entitiy

---
+++ auth_service/src/main/java/com/eureka/auth_service/model/User.java
@@ -0,0 +1,35 @@
+package com.eureka.auth_service.model;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import com.eureka.auth_service.type.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "Invalid email format"
+    )
+    @NotNull
+    private String email;
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character"
+    )
+    @NotNull
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
---
+++ auth_service/src/main/java/com/eureka/auth_service/type/Role.java
@@ -0,0 +1,10 @@
+package com.eureka.auth_service.type;
+
+public enum Role {
+    CUSTOMER,
+    ADMIN,
+    DELIVERY_PERSON,
+    EMPLOYEE,
+    INVENTORY_MANAGER,
+    SUPPORT_STAFF
+}
