939c1d0b9ba07b355eb39867150f40f16973202e
panchaliSam <panchalisam20030117@gmail.com>
2025-08-06 18:09:11 +0530

    Add dto response

---
+++ auth_service/src/main/java/com/eureka/auth_service/dto/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.eureka.auth_service.dto;
+
+import com.eureka.auth_service.type.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+}
